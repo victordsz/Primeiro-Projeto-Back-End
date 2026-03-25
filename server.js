e77c7c5e4191484939cf4885dee39526fe691f29
victor <victor88dsds88@gmail.com>
2026-03-24 21:33:23 -0300

    introdução a .env

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+
+// importando (.env)
+require('dotenv').config();
+
 // importando o express
 const express = require('express');
 
 // criando uma aplicação express, aqui inicializando o nosso servidor
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
+const PORT = process.env.PORT 
 
 // "/" significa rota principal (http://localhost:3000/)
 app.get("/", (req, res) =>{
 
     //req = request (dados enviados pelo cliente)
     //res = response (resposta que vamos enviar)
-    res.send("Olá Mundo");
+    res.send('Olá Mundo, passo 2');
 });
 
 app.listen(PORT, () =>{
